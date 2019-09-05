e958374454a8c8b644ad9f606541c24101253028
wk-j <somnuk.wk@gmail.com>
2019-09-05 14:34:18 +0700

    Locust

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-## Authentication
+## Custom Authentication
+
+```bash
+pipenv install locustio
+pipenv run locust --host=http://localhost:5000 -f locustfile.py
+open http://localhost:8089
+```
---
+++ locustfile.py
@@ -0,0 +1,20 @@
+from locust import HttpLocust, TaskSet, task
+
+class WebsiteTasks(TaskSet):
+    def on_start(self):
+        rest = self.client.get("/api/account/login/1234")
+        print(rest)
+
+    @task
+    def get_computers(self):
+        url = f"/api/data/getComputers"
+        self.client.get(url)
+
+    @task
+    def index(self):
+        self.client.get("/weatherForecast")
+
+class WebsiteUser(HttpLocust):
+    task_set = WebsiteTasks
+    min_wait = 5000
+    max_wait = 15000
